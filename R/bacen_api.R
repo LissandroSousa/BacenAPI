a52a3842491c2e95a16408e6f18ccb54defe0018
LissandroSousa <lisandrosousa@gmail.com>
2024-12-17 17:56:03 -0300

    first

---
+++ R/bacen_api.R
@@ -0,0 +1,107 @@
+#' Conexão com a API do Banco Central.
+#'
+#' Esta função estabelece uma conexão com a API do Banco Central do Brasil (BACEN),
+#' utilizando os pacotes `httr` ou `httr2`, para obter dados em formato JSON e convertê-los
+#' em um formato legível como data frames.
+#'
+#' @import $(1:httr) $(2:GET)
+#'
+#' @param url Um string contendo o URL da API do BACEN para a série desejada.
+#' @param httr Um valor lógico. Se `TRUE`, a função utiliza o pacote `httr` para a conexão.
+#' Caso contrário, utiliza o pacote `httr2`. O valor padrão é `TRUE`.
+#'
+#' @return Retorna os dados obtidos da API do BACEN.
+#'
+#' @examples
+#' # Exemplo usando o pacote httr:
+#' url <- "https://api.bcb.gov.br/dados/serie/bcdata.sgs.433/dados?formato=json&dataInicial=01/01/2003&dataFinal=31/12/2023"
+#' dados <- bacen_api(url, httr = TRUE)
+#'
+#' # Exemplo usando o pacote httr2:
+#' dados <- bacen_api(url, httr = FALSE)
+#'
+#'
+#' @export
+bacen_api <- function(url, httr = TRUE){
+  message('Iniciando a conexao com a API do Bacen\n')
+  flag = 0
+
+  # --- API Connection - Using httr --- #
+  if(httr == TRUE){
+
+    # -- API Connection -- #
+    api_connection = GET(url = url)
+
+    # --- Connection Flag --- #
+    if(api_connection$status_code == 200){
+      dlg_message(message = 'Conexao bem sucedida ! \nDados sendo coletados ...\n', type = 'ok')
+    }
+    else if(api_connection$status_code != 200){
+      while(api_connection$status_code != 200 & flag <= 3){
+        flag = flag + 1
+
+        if(flag == 1){
+          Sys.sleep(2)
+          message('Problemas na conexao. \nTentando acessar a API novamente ...\n')}
+        if(flag == 2){
+          Sys.sleep(5)
+          message('Problemas na conexao. \nTentando acessar a API novamente ...\n')}
+        if(flag == 3){
+          Sys.sleep(10)
+          message('Problemas na conexao. \nTentando acessar a API uma última vez ...\n')}
+
+        api_connection = GET(url = url)
+      }
+
+      ifelse(api_connection$status_code == 200,
+             dlg_message(message = 'Conexao bem sucedida ! \nDados sendo coletados ...\n', type = 'ok'),
+             dlg_message(message = 'Falha na conexao ! \nTente conectar com a API mais tarde.', type = 'ok')
+      )
+    }
+
+    # --- Converting Data to a Readable Format --- #
+    api_connection = rawToChar(api_connection$content)              # Raw to Json
+    api_connection = fromJSON(api_connection, flatten = TRUE)       # Json to Data Frame
+  }
+
+  # --- API Connection - Using httr2 --- #
+  else{
+
+    # -- API Connection -- #
+    api_connection = request(base_url = url) %>% req_perform()
+
+    # --- Connection Flag --- #
+    if(api_connection$status_code == 200){
+      dlg_message(message = 'Conexao bem sucedida ! \nDados sendo coletados ...\n', type  = 'ok')
+    }
+    else if(api_connection$status_code != 200){
+      while(api_connection$status_code != 200 & flag <= 3){
+        flag = flag + 1
+
+        if(flag == 1){
+          Sys.sleep(2)
+          message('Problemas na conexao. \nTentando acessar a API novamente ...\n')}
+        if(flag == 2){
+          Sys.sleep(5)
+          message('Problemas na conexao. \nTentando acessar a API novamente ...\n')}
+        if(flag == 3){
+          Sys.sleep(10)
+          message('Problemas na conexao. \nTentando acessar a API uma última vez ...\n')}
+
+        api_connection = request(base_url = url) %>% req_perform()
+      }
+
+      ifelse(api_connection$status_code == 200,
+             dlg_message(message = 'Conexao bem sucedida ! \nDados sendo coletados ...\n', type = 'ok'),
+             dlg_message(message = 'Falha na conexao ! \nTente conectar com a API mais tarde.', type = 'ok')
+      )
+    }
+
+    # --- Converting Data to a Readable Format --- #
+    api_connection = rawToChar(api_connection$body)                 # Raw to JSon
+    api_connection = fromJSON(api_connection, flatten = TRUE)       # Json to Data Frame
+  }
+
+  # --- Output --- #
+  return(api_connection)
+}
---
+++ R/bacen_url.R
@@ -0,0 +1,26 @@
+#' Criação de URLs para acesso à API do Banco Central.
+#'
+#' Esta função gera URLs que permitem acessar dados de séries temporais específicas
+#' fornecidas pela API do Banco Central do Brasil (BACEN).
+#'
+#' @param serie Código(s) da(s) série(s) desejada(s). Deve ser um número ou vetor de números.
+#' @param data_inicio Data inicial do período de interesse, no formato "dd/mm/aaaa".
+#' @param data_termino Data final do período de interesse, no formato "dd/mm/aaaa".
+#'
+#' @return Retorna um vetor contendo os URLs criados para cada série fornecida.
+#'
+#' @examples
+#' # Gerar URL para a série 433 (IPCA) no período de 01/01/2003 a 31/12/2023.
+#' bacen_url(433, "01/01/2003", "31/12/2023")
+#'
+#' @export
+bacen_url <- function(serie, data_inicio, data_termino){
+  url = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.'
+
+  for(i in serie){
+    bacen_url = paste0(url, serie, '/dados?formato=json&dataInicial=', data_inicio, '&dataFinal=', data_termino)
+  }
+
+  return(bacen_url)
+}
+
